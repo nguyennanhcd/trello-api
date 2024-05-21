6a3a53604d284c2d67d88c884cccc981e6d71b88
nguyenanhcd <anh487303@gmail.com>
2024-05-21 18:46:01 +0700

    express_router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,14 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoutes } from './boardRoutes'
+
+const Router = express.Router()
+
+// check API v1 status
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'APIs V1 are ready to use' })
+})
+
+Router.use('/boards', boardRoutes)
+
+export const APIs_V1 = Router
